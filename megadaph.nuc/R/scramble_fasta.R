0b1ba6aa4f7a786a9e1243348a1eb34c2a29fa1a
Fenner Macrae <fennermacrae@gmail.com>
2017-08-26 13:27:42 -0700

    Added docopt to scramble_fasta

--- megadaph.nuc/R/scramble_fasta.R
+++ megadaph.nuc/R/scramble_fasta.R
@@ -1,18 +1,24 @@
 #!/usr/bin/Rscript
 
+'Randomize the nucleotide order for each sequence in a fasta file
+
+Usage: scramble_fasta.R [-o PATH] [--help] FASTA ...
+
+-h --help    show this
+-o --outdir=PATH    Output Directory (Default: Current directory)' -> doc
+
+
 #' Scramble a fasta file
 #'
-#' For each sequence in the file, randomly scramble the sequence and write to
-#' file.
+#' For each sequence in the file, randomly scramble the sequence
 #' @param fasta Input fasta file
-#' @param outfile Output file path
+#' @return A scrambled DNAstring object
 #' @importFrom fen.R.util read_text ulapply
-#' @importFrom Biostrings readDNAStringSet DNAStringSet writeXStringSet
+#' @importFrom Biostrings readDNAStringSet DNAStringSet
 #' @export
-scramble_fasta <- function(fasta, outfile) {
+scramble_fasta <- function(fasta) {
     fasta_string <- readDNAStringSet(fasta)
-    shuffled <- DNAStringSet(lapply(fasta_string, sample))
-    writeXStringSet(shuffled, outfile)
+    DNAStringSet(lapply(fasta_string, sample))
 }
 
 # If run as script, scramble all command line arguments
@@ -20,11 +26,22 @@ if (!interactive()) {
     library(Biostrings)
     library(fen.R.util)
     library(tools)
-    args = commandArgs(trailingOnly = TRUE)
-    outdir <- "scrambled"
-    dir.create(outdir, showWarnings = FALSE)
-    lapply(args, function(x) {
-        outfile <- paste0(file_path_sans_ext(x), ".scrambled.fasta")
-        scramble_fasta(x, outfile)
+    library(docopt)
+
+    opts <- docopt(doc)
+
+    # Set output directory
+    if (is.null(opts$`--outdir`)) {
+        outdir <- getwd()
+    } else {
+        outdir <- opts$`--outdir`
+    }
+
+    lapply(opts$FASTA, function(x) {
+
+        outfile <- paste0(outdir, "/", file_path_sans_ext(x),
+                          ".scrambled.fasta")
+        scrambled <- scramble_fasta(x)
+        writeXStringSet(scrambled, outfile)
     })
 }
