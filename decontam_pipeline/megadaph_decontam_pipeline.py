a5449a2b19dfa45419d4ab36b1f51db64b48a97d
Fenner Macrae <fennermacrae@gmail.com>
2017-11-02 11:53:29 -0700

    Intermediate and unbuggy

--- decontam_pipeline/megadaph_decontam_pipeline.py
+++ decontam_pipeline/megadaph_decontam_pipeline.py
@@ -8,7 +8,7 @@ Pipeline steps are as follows:
 
 Usage:
     megadaph_decontam_pipeline.py [--flowchart=<file.svg>] [--verbose]
-        [--just-print] (--threads=<n>) (--assemblies=<directory>)
+        [--just-print] (--threads=<n>)
         (--fwd_reads=<directory>) (--rev_reads=<directory>)
     megadaph_decontam_pipeline.py (-h | --help)
 
@@ -27,33 +27,60 @@ reads
 reads. Can be the same as x - direction will be distinguished using 1./2. in
 filenames.
 
-
 Required Input Files:
     1. A set of paired Fastq files
-    2. A corresponding set of assemblies produced from the Fastq files in
-    Fasta format
 
 Notes:
-    Matched Fastq and Fasta files must share the same prefix and be named using
-    dot filename convention (E.g FA_SC.1.trim.fastq). Forward reads must have
-    '.1' immediately after shared prefix, reverse reads must have '.2'.
+    Matched Fastq files must share the same prefix and be named using dot
+    filename convention (E.g FA_SC.1.trim.fastq). Forward reads must have '.1'
+    immediately after shared prefix, reverse reads must have '.2'.
 """
 
 from pathlib import Path
-from typing import Any
-from typing import Dict
-from typing import List
-from typing import Tuple
+from typing import (
+        Any,
+        Dict,
+        Iterable,
+        List,
+        )
 
 from docopt import docopt
-import ruffus
+from ruffus import (
+        formatter,
+        Pipeline,
+        ruffus_utility,
+        suffix,
+        )
+from fmbiopy.biofile import (
+        BiofileGroup,
+        Fastq,
+        MatchedPrefixGroup,
+        )
+from fmbiopy.fmpaths import (
+        as_strs,
+        find,
+        )
+from fmbiopy.fmruffus import (
+        apply_,
+        Centrifuge,
+        format_input,
+        output_format,
+        PairedBowtie2Align,
+        SymlinkInputs,
+        )
 
-import fmbiopy.biofile as biofile
-import fmbiopy.fmpaths as fmpaths
-import fmbiopy.fmruffus as fmruffus
+"""The extra parameters for each task in the pipeline"""
+PARAM = {
+        'PairedBowtie2Align' : ['-X', '1000'],
+        }
 
+"""The working directory"""
+WD = Path.cwd()
 
-def _get_gzip_status(files: List[Path]) -> bool:
+"""The output directory"""
+OUTPUT_DIR = WD / 'pipe'
+
+def _get_gzip_status(files: Iterable[Path]) -> bool:
     """Get the gzip status of a group of files
 
     Files are expected to have the same Gzip status.
@@ -77,7 +104,6 @@ def _get_gzip_status(files: List[Path]) -> bool:
 
 
 def _validate_args(
-        assemblies: List[Path],
         fwd_reads: List[Path],
         rev_reads: List[Path],
         )-> None:
@@ -90,22 +116,19 @@ def _validate_args(
     ----------
     fwd_reads, rev_reads
         The user inputted fastq file paths
-    assemblies
-        The user inputted assembly files
     """
 
-    inputs = [fwd_reads, rev_reads, assemblies]
-    gzipped = [_get_gzip_status(input) for input in inputs]
-    biofile.MatchedPrefixGroup([
-        biofile.BiofileGroup(fwd_reads, filetype='fastq', gzipped=gzipped[0]),
-        biofile.BiofileGroup(rev_reads, filetype='fastq', gzipped=gzipped[1]),
-        biofile.BiofileGroup(assemblies, filetype='fasta', gzipped=gzipped[2])
+    inputs = [fwd_reads, rev_reads]
+    gzipped = [_get_gzip_status(inp) for inp in inputs]
+    MatchedPrefixGroup([
+        BiofileGroup(fwd_reads, filetype='fastq', gzipped=gzipped[0]),
+        BiofileGroup(rev_reads, filetype='fastq', gzipped=gzipped[1])
         ]).validate()
 
 
 def _extract_input_files(
         opt: Dict[str, Any]
-        )-> List[Tuple[Path, Path, Path]]:
+        )-> Dict[str, List[Path]]:
     """Extract the input files from the given directories
 
     Input file prefixes are checked to ensure they can be matched correctly and
@@ -118,42 +141,40 @@ def _extract_input_files(
 
     Returns
     -------
-    A dictionary of the given input files. Returns None if no input files were
-    given.
+    A dictionary of the input files
     """
 
     # Gather the inputs
-    inputs = [
-            opt['--assemblies'], opt['--fwd_reads'], opt['--rev_reads']]
+    inputs = [opt['--fwd_reads'], opt['--rev_reads']]
 
     # Extract input files
     input_dirs = [Path(d).resolve() for d in inputs]
 
     # The expected filetypes
-    extensions = [
-            biofile.Fasta.accepted_extensions,
-            biofile.Fastq.accepted_extensions,
-            biofile.Fastq.accepted_extensions]
+    extensions = [Fastq.accepted_extensions] * 2
     # The expected substrings
-    substrings = [None, '1.', '2.']
+    substrings = ['1.', '2.']
 
     # Get the paths to all input files
     file_paths: List[List[Path]] = []
     for path, typ, substring in zip(input_dirs, extensions, substrings):
-        file_paths.append(fmpaths.find(path, typ, substring))
+        file_paths.append(find(path, typ, substring))
 
-    assemblies = file_paths[0]
-    fwd_reads = file_paths[1]
-    rev_reads = file_paths[2]
-    _validate_args(assemblies, fwd_reads, rev_reads)
+    fwd_reads = file_paths[0]
+    rev_reads = file_paths[1]
+    _validate_args(fwd_reads, rev_reads)
 
-    return list(zip(assemblies, fwd_reads, rev_reads))
+    output_dict = {
+            'fwd_reads' : fwd_reads,
+            'rev_reads' : rev_reads}
+
+    return output_dict
 
 
 def _define_pipeline(
-        input_files: List[Tuple[Path, Path, Path]],
+        input_files: Dict[str, List[Path]],
         name: str,
-        )-> ruffus.Pipeline:
+        )-> Pipeline:
     """Define the pipeline
 
     Parameters
@@ -167,18 +188,49 @@ def _define_pipeline(
     -------
     The pipeline definition
     """
-    path_strings = [fmpaths.as_str(paths) for paths in input_files]
-    # Define the pipeline
-    pipe = ruffus.Pipeline(name=name)
+    path_strings = [as_strs(paths) for paths in input_files.values()]
+    fwd_reads = path_strings[0]
+    rev_reads = path_strings[1]
+    gzipped = _get_gzip_status(input_files['fwd_reads'])
 
-    # Add symlink step
-    pipe.transform(
-            task_func=fmruffus.PairedBowtie2Align,
-            name='alignment',
-            filter=ruffus.suffix('fasta'),
-            input=path_strings,
-            output='bam',
-            output_dir='PairedBowtie2align')
+    link_suffixes = (['.R1.fastq', '.R2.fastq'])
+    if gzipped:
+        link_suffixes = [''.join([suff, '.gz']) for suff in link_suffixes]
+
+    # Define the pipeline
+    pipe = Pipeline(name=name)
+
+    # Symlink Inputs
+    fwd_read_links = pipe.transform(
+            task_func=SymlinkInputs().run,
+            filter=format_input(Fastq.accepted_extensions),
+            input=fwd_reads,
+            output=output_format([link_suffixes[0]], dirname='symlink_inputs'))
+    rev_read_links = pipe.transform(
+            task_func=SymlinkInputs().run,
+            filter=format_input(Fastq.accepted_extensions),
+            input=rev_reads,
+            output=output_format([link_suffixes[1]], dirname='symlink_inputs'))
+    # Run Fastqc
+    # Trim Reads
+    # Rerun Fastqc
+    # Summarize Trimming
+    # Assemble Reads
+    # Build Centrifuge Database
+    # Simulate Minimum hit length
+    # Run Centrifuge
+    # cent = pipe.transform(
+    #         task_func=Centrifuge().run,
+    #         filter=suffix('fasta'),
+    #         output='tsv',
+    #         output_dir='centrifuge')
+    # align = pipe.transform(
+    #         task_func=PairedBowtie2Align(PARAM['PairedBowtie2Align']).run,
+    #         name='Alignment',
+    #         filter=suffix('fasta'),
+    #         input=[assembly_links, fwd_read_links, rev_read_links],
+    #         output='bam',
+    #         output_dir='align_to_assemblies')
     return pipe
 
 
@@ -192,26 +244,25 @@ def _main(opt: Dict[str, Any], name: str = 'decontam') -> None:
     ----------
     opt
         A dictionary of parsed input parameters formatted as in `docopt`
-    name: Optional
+    name: optional
         The name of the pipeline
     """
-    # Unpack the arguments
+    # Unpack arguments
     if opt['--threads']:
         nthreads = int(opt['--threads'])
-
     input_files = _extract_input_files(opt)
 
+    # Define the pipeline
     pipe = _define_pipeline(input_files, name=name)
 
+    # Run or print pipeline
     if opt['--flowchart']:
         pipe.printout_graph(opt['--flowchart'])
-
     if opt['--just-print']:
         pipe.printout()
     elif input_files:
-        pipe.run(
-            multithread=nthreads,
-            logger=fmruffus.ROOT_LOGGER)
+        pipe.run(multithread=nthreads, exceptions_terminate_immediately=True)
+
 
 if __name__ == '__main__':
     opt = docopt(__doc__)
--- decontam_pipeline/test/test_megadaph_decontam_pipeline.py
+++ decontam_pipeline/test/test_megadaph_decontam_pipeline.py
@@ -1,8 +1,9 @@
 """Test the Megadaph Decontamination Pipeline using py.test"""
-from uuid import uuid4
+import uuid
 
 from docopt import docopt
 import pytest
+from ruffus import formatter
 
 import fmbiopy.fmlist as fmlist
 from fmbiopy.fmtest import *
@@ -16,8 +17,8 @@ def doc():
 
 def test_docopt_parsing(doc):
     expected_keys = [
-            '--threads', '--assemblies', '--fwd_reads', '--rev_reads']
-    values = '10', 'foo', 'bar', 'foobar'
+            '--threads', '--fwd_reads', '--rev_reads']
+    values = '10', 'bar', 'foobar'
     argv = fmlist.interleave(expected_keys, values)
     parsed = docopt(doc, argv)
     for opt, val in zip(expected_keys, values):
@@ -25,16 +26,13 @@ def test_docopt_parsing(doc):
 
 
 @pytest.fixture()
-def opts():
-    # TODO: refactor to use dat keys
-    location = Path('sandbox/small')
-    subdirs = ['assemblies', 'fwd_reads', 'rev_reads']
-    paths = [str((location / d).resolve()) for d in subdirs]
+def opts(small):
+    subdirs = ['fwd_reads', 'rev_reads']
+    paths = [str((small / d).resolve()) for d in subdirs]
     return {
             '--threads' : 1,
-            '--assemblies' : paths[0],
-            '--fwd_reads' : paths[1],
-            '--rev_reads' : paths[2],
+            '--fwd_reads' : paths[0],
+            '--rev_reads' : paths[1],
             '--flowchart' : False,
             '--just-print' : False,
             '--help' : False}
@@ -47,40 +45,34 @@ def print_only_opts(opts):
 
 
 @pytest.fixture()
-def flowchart_only_opts(print_only_opts):
-    print_only_opts['--flowchart'] = 'sandbox/flow.svg'
+def flowchart_only_opts(print_only_opts, sandbox):
+    print_only_opts['--flowchart'] = str(sandbox / 'flow.svg')
     return print_only_opts
 
 
 @pytest.fixture()
-def zipped_dat_inputs(dat):
-    return list(zip(
-        sorted(dat['small']['assemblies']),
-        sorted(dat['small']['fwd_reads']),
-        sorted(dat['small']['rev_reads'])))
+def input_file_dict(dat):
+    return {
+            'fwd_reads' : sorted(dat['small']['fwd_reads']),
+            'rev_reads' : sorted(dat['small']['rev_reads'])
+    }
 
 
-@pytest.fixture()
-def uid():
-    return uuid4().hex
+def test_check_input_files(opts, input_file_dict):
+    assert pipe._extract_input_files(opts) == input_file_dict
 
 
-def test_check_input_files(opts, zipped_dat_inputs):
-    assert pipe._extract_input_files(opts) == zipped_dat_inputs
-
-
-def test_flowchart_only(flowchart_only_opts, uid):
-    pipe._main(flowchart_only_opts, name=uid)
+def test_flowchart_only(flowchart_only_opts, randstr):
+    pipe._main(flowchart_only_opts, name=randstr())
     assert Path(flowchart_only_opts['--flowchart']).exists()
 
 
-def test_print_only(print_only_opts, uid):
-    pytest.set_trace()
-    pipe._main(print_only_opts, name=uid)
+def test_print_only(print_only_opts, randstr):
+    pipe._main(print_only_opts, name=randstr())
 
 
 def test_logging():
     pass
 
-# def test_run_pipeline(opts):
-#     _main(opts)
+def test_run_pipeline(opts):
+    pipe._main(opts)
