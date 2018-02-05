fb7d8fc42a645390d54346566826c806faee5764
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-02-05 10:32:15 -0800

    Added blobtools view to pipeline

--- pipe/scripts/produce_covplots.py
+++ pipe/scripts/produce_covplots.py
@@ -2,10 +2,11 @@
 from plumbum import local
 from snakemake import shell
 
-shell("blobtools covplot -p 20 -c {input.covplot} -i {input.db} -o {sample}")
+shell("blobtools covplot -p 20 -c {snakemake.input.covplot} -i "
+      "{snakemake.input.db} -o {snakemake.params.output_prefix}")
 outdir = local.path("output/produce_covplots1")
 output_files = outdir.glob(snakemake.wildcards.sample + "*")
 for f in output_files:
-    extensions = ''.join(f.split(".")[2:])
-    newname = outdir / (snakemake.wildcards.sample + extensions)
+    extensions = '.'.join(f.name.split(".")[2:])
+    newname = outdir / (snakemake.wildcards.sample + "." + extensions)
     f.move(newname)
