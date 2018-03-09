e59e80ba44cd85f28da1c3822d98959321f434e1
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-09 09:41:05 -0800

    Updated busco config to try to fix augustus errors

--- pipe/scripts/busco.py
+++ pipe/scripts/busco.py
@@ -16,6 +16,9 @@ outdir_root = outdir.dirname
 local.env['BUSCO_CONFIG_FILE'] = str(local.path(snakemake.config['busco_config']))
 local.env['AUGUSTUS_CONFIG_PATH'] = str(local.path(
     snakemake.config['augustus_config']))
+local.env['PATH'] = ':'.join([
+    local.path(snakemake.config['augustus_scripts']),
+    local.env['PATH']])
 
 assembly = local.path(snakemake.input)
 lineage_file = local.path(snakemake.config['busco_database'])
@@ -23,9 +26,9 @@ tmp_outdir = outdir_root / ('run_' + snakemake.wildcards.genotype)
 
 with local.cwd(outdir_root):
     busco['-i', assembly, '-o', snakemake.wildcards.genotype, "-l",
-          lineage_file, '-m', 'geno', '-f'] & FG
+          lineage_file, '-m', 'geno', '-sp', 'fly', '-f'] & FG
 
-for f in tmp_outdir.list():
-    f.move(outdir)
+    for f in tmp_outdir.list():
+        f.move(outdir)
 
-tmp_outdir.delete()
+    tmp_outdir.delete()
