eac2b13233802327a89ce073f4bc0cb2e16ac0ae
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-04-18 13:19:16 -0700

    Added QUAL plotting

--- pipe/scripts/plot_variant_quality_scores.R
+++ pipe/scripts/plot_variant_quality_scores.R
@@ -22,25 +22,32 @@ vcf$variant <- factor(
 snp_color <- "#A9E2E4"
 indel_color <- "#F4CCCA"
 
+QUAL <- ggplot(vcf, aes(x = QUAL, fill = variant)) +
+  geom_density(alpha = 0.3) +
+  geom_vline(xintercept = 100, size = 0.7)
+
 DP <- ggplot(vcf, aes(x = DP, fill = variant)) +
   geom_density(alpha = 0.3) +
-  geom_vline(xintercept = c(10, 6200))
+  geom_vline(xintercept = c(10, 100)) +
+  xlim(0, 1000)
 
 QD <- ggplot(vcf, aes(x = QD, fill = variant)) +
   geom_density(alpha = 0.3) +
-  geom_vline(xintercept = 2, size = 0.7)
+  geom_vline(xintercept = 5, size = 0.7)
 
 FS <- ggplot(vcf, aes(x = FS, fill = variant)) +
   geom_density(alpha = 0.3) +
-  geom_vline(xintercept = c(60, 200), size = 0.7) + ylim(0,0.1)
+  geom_vline(xintercept = c(60, 200), size = 0.7) +
+  ylim(0,0.1) +
+  xlim(0, 250)
 
 MQ <- ggplot(vcf, aes(x = MQ, fill = variant)) +
   geom_density(alpha = 0.3) +
-  geom_vline(xintercept = 40, size = 0.7)
+  geom_vline(xintercept = 35, size = 0.7)
 
 MQRankSum <- ggplot(vcf, aes(x = MQRankSum, fill = variant)) +
   geom_density(alpha = 0.3) +
-  geom_vline(xintercept = -20, size = 0.7)
+  geom_vline(xintercept = -12.5, size = 0.7)
 
 SOR <- ggplot(vcf, aes(x=SOR, fill=variant)) +
   geom_density(alpha = 0.3) +
@@ -49,10 +56,9 @@ SOR <- ggplot(vcf, aes(x=SOR, fill=variant)) +
 ReadPosRankSum <- ggplot(vcf, aes(x = ReadPosRankSum, fill = variant)) +
   geom_density(alpha = 0.3) +
   geom_vline(
-      xintercept = c(-10, 10, -20, 20),
-      size = 1,
-      colour = c(snp_color, snp_color, indel_color, indel_color)) +
-  xlim(-30, 30)
+      xintercept = c(-10, 10),
+      size = 0.7) +
+  xlim(-15, 15)
 
 svg(snakemake@output[[1]], height = 20, width = 15)
 theme_set(theme_gray(base_size = 18))
