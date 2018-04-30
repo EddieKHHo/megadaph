489b5d2d3dafafc92632cb591a69bb4ec2c20518
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-04-29 17:25:35 -0700

    Removed haplomerger scripts

--- pipe/scripts/tile_blobplots.sh
+++ pipe/scripts/tile_blobplots.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-montage output/produce_blobplots3/*/phylum/*blobplot.covsum.png \
+tmpdir=/tmp/tiled_blobplots
+mkdir $tmpdir
+
+for img in output/produce_covplots_reduced/*.covsum.png; do
+    convert "$img" -resize 300x300 $tmpdir/"$(basename "$img")"
+done
+
+montage "$tmpdir/*.covsum.png" \
     -scale 1000x1000 \
     -mode Concatenate \
-    -tile 3x3 montage.png
+    -tile 3x3 covplots_tiled.png
+
+rm -rf $tmpdir
+
+montage output/produce_blobplots_reduced/*/phylum/*blobplot.covsum.png \
+    -scale 1000x1000 \
+    -mode Concatenate \
+    -tile 3x3 blobplots_tiled.png
