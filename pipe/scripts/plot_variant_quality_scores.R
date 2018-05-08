e7f1c6ab0c8c16237709c08e9b479f43f95d721f
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-05-08 12:59:51 -0700

    Added mutant allele count plot

--- pipe/scripts/plot_variant_quality_scores.R
+++ pipe/scripts/plot_variant_quality_scores.R
@@ -1,26 +1,25 @@
 library("gridExtra")
 library("ggplot2")
+library("tidyr")
 
 input_dir <- dirname(snakemake@input[[1]])
 output_dir <- dirname(snakemake@output[[1]])
 
-vcf_snps <- read.csv(
-    snakemake@input[["snps"]],
-    header = TRUE,
-    na.strings=c("", "NA"),
-    sep = "\t")
-vcf_indels <- read.csv(
-    snakemake@input[["indels"]],
-    header = TRUE,
-    na.strings=c("","NA"),
-    sep = "\t")
+vcf_snps <- read_tsv(snakemake@input[["snps"]])
+vcf_indels <- read_tsv(snakemake@input[["indels"]])
 vcf <- rbind(vcf_snps, vcf_indels)
 vcf$variant <- factor(
     c(rep("SNPs", dim(vcf_snps)[1]),
       rep("Indels", dim(vcf_indels)[1])))
+ad_col = cols[which(map_lgl(cols, ~grepl('AD', .)))]
+vcf <- vcf %>%
+  separate(col=ad_col, into=c('MUT', 'WT')) %>%
+  mutate(MUT = as.integer(MUT))
 
 snp_color <- "#A9E2E4"
 indel_color <- "#F4CCCA"
+
+vcf %>% separate(col='')
 
 QUAL <- ggplot(vcf, aes(x = QUAL, fill = variant)) +
   geom_density(alpha = 0.3) +
@@ -61,7 +60,11 @@ ReadPosRankSum <- ggplot(vcf, aes(x = ReadPosRankSum, fill = variant)) +
       size = 0.7) +
   xlim(-15, 15)
 
+AD <- ggplot(vcf, aes(x=MUT, fill=variant)) +
+  geom_density(alpha = 0.3) +
+  xlim(0, 100)
+
 svg(snakemake@output[[1]], height = 20, width = 15)
 theme_set(theme_gray(base_size = 18))
-grid.arrange(QUAL, QD, DP, FS, MQ, MQRankSum, SOR, ReadPosRankSum, nrow = 4)
+grid.arrange(QUAL, QD, DP, FS, MQ, MQRankSum, SOR, ReadPosRankSum, AD, nrow = 5)
 dev.off()
