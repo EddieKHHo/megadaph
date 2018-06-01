cec59ee3c91e5c525c68537095fb62241a47a01d
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-06-01 14:26:35 -0700

    Updated fmbiopy imports to new module names

--- pipe/scripts/rename_megadaph_files.py
+++ pipe/scripts/rename_megadaph_files.py
@@ -8,28 +8,28 @@ lane8-s046-indexD711-D501-TCTCGCGC-AGGCTATA-A10_73_FA7_S46_L008_R2_001.fq.gz
 -> FA7.R2.fq.gz
 """
 from sys import argv
-from typing import Sequence
 
-from fmbiopy.fmpaths import as_paths
+from plumbum import local
 
-def rename_megadaph_files(files: Sequence[str]):
+
+def rename_megadaph_files(files):
     """Rename the original Megadaph files, using a simpler naming convention"""
     # Convert to a list of paths
-    paths = as_paths(files)
+    paths = [local.path(file) for file in files]
     for path in paths:
-        suffix = ''.join(path.suffixes)
+        suffix = "".join(path.suffixes)
         name = path.name
-        unscore_split = name.split('_')
+        unscore_split = name.split("_")
         sample = unscore_split[2]
 
         # Check if file is a control, which have to be handled differently
-        if any(sub in name for sub in ['SC', 'EC']):
+        if any(sub in name for sub in ["SC", "EC"]):
             sample += unscore_split[3]
             pair = unscore_split[6]
         else:
             pair = unscore_split[5]
 
-        new_name = ''.join([sample, '.', pair, suffix])
+        new_name = "".join([sample, ".", pair, suffix])
         path.rename(new_name)
 
 
--- pipe/scripts/test/test_filter_mitochondrial_scaffolds.py
+++ pipe/scripts/test/test_filter_mitochondrial_scaffolds.py
@@ -6,8 +6,8 @@ from Bio.Alphabet import IUPAC
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from click.testing import CliRunner
-from fmbiopy.fmbio import rand_dna_seq
-from fmbiopy.fmsystem import capture_stdout
+from fmbiopy.bio import rand_dna_seq
+from fmbiopy.system import capture_stdout
 from plumbum import local
 from plumbum.cmd import list_csomes
 from pytest import fixture
@@ -16,60 +16,85 @@ from filter_mitochondrial_scaffolds import main
 
 
 @fixture(
-    scope='session',
-    params=[dict(MT=15000, NOT_MT1=150),
-            dict(NOT_MT1=900, NOT_MT2=150),
-            dict(MT=13000, NUMT=10000)],
-    ids=['single_hit', 'no_hit', 'numt_hit'])
+    scope="session",
+    params=[
+        dict(MT=15000, NOT_MT1=150),
+        dict(NOT_MT1=900, NOT_MT2=150),
+        dict(MT=13000, NUMT=10000),
+    ],
+    ids=["single_hit", "no_hit", "numt_hit"],
+)
 def blast_tsv(request, tempdir):
-    tmpfile = local.path(str(tempdir)) / (uuid4().hex + '.tsv')
-    with tmpfile.open('w') as tsv:
-        writer = csv.writer(tsv, delimiter='\t')
+    tmpfile = local.path(str(tempdir)) / (uuid4().hex + ".tsv")
+    with tmpfile.open("w") as tsv:
+        writer = csv.writer(tsv, delimiter="\t")
         for name, length in request.param.items():
-            writer.writerow(['FI-XINB3_mtDNA', name, '99.699', length,
-                             '45', '0', '1', '14969', '59042', '74010', '0.0',
-                             '27394'])
+            writer.writerow(
+                [
+                    "FI-XINB3_mtDNA",
+                    name,
+                    "99.699",
+                    length,
+                    "45",
+                    "0",
+                    "1",
+                    "14969",
+                    "59042",
+                    "74010",
+                    "0.0",
+                    "27394",
+                ]
+            )
     return dict(file=tmpfile, params=request.param)
 
 
-@fixture(scope='module')
+@fixture(scope="module")
 def assembly(tempdir):
-    tmpfile = local.path(str(tempdir)) / (uuid4().hex + '.fa')
+    tmpfile = local.path(str(tempdir)) / (uuid4().hex + ".fa")
 
     scaffolds = [
-        SeqRecord(Seq(rand_dna_seq(16000), IUPAC.ambiguous_dna), id='MT'),
-        SeqRecord(Seq(rand_dna_seq(20000), IUPAC.ambiguous_dna), id='NOT_MT1'),
-        SeqRecord(Seq(rand_dna_seq(20000), IUPAC.ambiguous_dna), id='NOT_MT2'),
-        SeqRecord(Seq(rand_dna_seq(50000), IUPAC.ambiguous_dna), id='NUMT')
+        SeqRecord(Seq(rand_dna_seq(16000), IUPAC.ambiguous_dna), id="MT"),
+        SeqRecord(Seq(rand_dna_seq(20000), IUPAC.ambiguous_dna), id="NOT_MT1"),
+        SeqRecord(Seq(rand_dna_seq(20000), IUPAC.ambiguous_dna), id="NOT_MT2"),
+        SeqRecord(Seq(rand_dna_seq(50000), IUPAC.ambiguous_dna), id="NUMT"),
     ]
 
-    with tmpfile.open('w') as f:
+    with tmpfile.open("w") as f:
         for scaffold in scaffolds:
-            SeqIO.write(scaffold, f, 'fasta')
+            SeqIO.write(scaffold, f, "fasta")
 
     return tmpfile
 
 
 def test_filter_mitochondrial_scaffolds(blast_tsv, assembly, tempdir):
-    outfasta = tempdir / (uuid4().hex + '.fa')
-    outtxt = tempdir / (uuid4().hex + '.txt')
+    outfasta = tempdir / (uuid4().hex + ".fa")
+    outtxt = tempdir / (uuid4().hex + ".txt")
     runner = CliRunner()
-    runner.invoke(main, ['--blast', str(blast_tsv['file']),
-                         '--assembly', str(assembly),
-                         '--outfasta', str(outfasta),
-                         '--outtxt', str(outtxt)])
+    runner.invoke(
+        main,
+        [
+            "--blast",
+            str(blast_tsv["file"]),
+            "--assembly",
+            str(assembly),
+            "--outfasta",
+            str(outfasta),
+            "--outtxt",
+            str(outtxt),
+        ],
+    )
     output_scaffolds = capture_stdout(list_csomes[outfasta])
 
-    with outtxt.open('r') as f:
+    with outtxt.open("r") as f:
         filtered_scaffolds = f.readlines()
         filtered_scaffolds = [x.rstrip() for x in filtered_scaffolds]
 
-    for seqid in ['NOT_MT1', 'NOT_MT2', 'NUMT']:
+    for seqid in ["NOT_MT1", "NOT_MT2", "NUMT"]:
         assert seqid in output_scaffolds
         assert seqid not in filtered_scaffolds
-    if 'MT' in blast_tsv['params'].keys():
-        assert 'MT' not in output_scaffolds
-        assert 'MT' in filtered_scaffolds
+    if "MT" in blast_tsv["params"].keys():
+        assert "MT" not in output_scaffolds
+        assert "MT" in filtered_scaffolds
     else:
-        assert 'MT' in output_scaffolds
-        assert 'MT' not in filtered_scaffolds
+        assert "MT" in output_scaffolds
+        assert "MT" not in filtered_scaffolds
